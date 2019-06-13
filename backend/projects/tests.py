e8dc57bd56107f91d502d01d765397f034860f3a
wstenzel <wills.stenzel@gmail.com>
2019-06-13 19:56:22 -0400

    Got test passing for testing an unauthenticated user

--- backend/projects/tests.py
+++ backend/projects/tests.py
@@ -1,17 +1,17 @@
 from django.test import TestCase, RequestFactory
-from graphene.test import Client
-from django.contrib.auth.models import User
+from graphene.test import Client, GraphQLError
+from django.contrib.auth.models import User, AnonymousUser
 from .models import Project, Vote
 
 from oasis_app.schema import schema
 
 class VoteTest(TestCase):
 
-    def login_test_user(test_case):
-        test_case.user = User.objects.create_user(username='test', password='test')
-        test_case.user.save()
-        test_case.client.login(username='test', password='test')
-        return test_case.user
+    def login_test_user(self):
+        self.user = User.objects.create_user(username='test', password='test')
+        self.user.save()
+        self.client.login(username='test', password='test')
+        return self.user
 
     def setUp(self):
         test_user = self.login_test_user()
@@ -23,7 +23,7 @@ class VoteTest(TestCase):
 
         Vote.objects.create(type='U', project=oasis, creator=test_user)
 
-
+    """ Tests that querying the list of votes works correctly """
     def test_get_votes(self):
         query = """
         query {
@@ -45,6 +45,7 @@ class VoteTest(TestCase):
         result = client.execute(query)
         self.assertEqual(expected, result['data'])
 
+    """ Tests that a user can create a vote """
     def test_create_vote(self):
         query = """
         mutation {
@@ -63,9 +64,38 @@ class VoteTest(TestCase):
             }
         }
         request_factory = RequestFactory()
+        request_factory.user = self.user
         context_value = request_factory.get('/')
-        context_value.user = User.objects.create(username='test1', password='test')
-        client = Client(schema)
+        context_value.user = self.user
+        client = Client(schema, context_value=context_value)
 
-        result = client.execute(query, context_value=context_value)
+        result = client.execute(query)
         self.assertEqual(expected, result['data'])
+
+    """ Tests that an unauthenticated user will not be allowed to create a vote"""
+    def test_unauthenticated_user_create_vote(self):
+        query = """
+        mutation {
+          createVote(
+            projectId:1
+            voteType: U) {
+            id
+            type
+          }
+        }
+        """
+        expected = {
+            "createVote": {
+                "id": 2,
+                "type": "Up"
+            }
+        }
+        request_factory = RequestFactory()
+        request_factory.user = AnonymousUser()
+        context_value = request_factory.get('/')
+        context_value.user = AnonymousUser()
+        client = Client(schema)
+        try:
+            result = client.execute(query, context_value=context_value)
+        except GraphQLError:
+            self.assertEqual('User must be logged in to vote', GraphQLError.message)
