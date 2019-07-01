6e2ab5cae7596037674ab426fc2b4a6f7130fb8d
Sam Xifaras <s.xifaras999@gmail.com>
2019-07-01 16:06:39 -0400

    Added foundation for frontend tests

---
+++ frontend/src/test/ProjectFeed.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { create } from "react-test-renderer";
+import Button from "../Button";
+
+import ProjectFeed from '../components/ProjectFeed';
+
+describe('Project Feed component tests', () => {
+    it('Project Feed renders correctly given mock data', () => {
+        // TODO
+    });
+});
